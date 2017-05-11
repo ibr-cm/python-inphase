320b0688a066babfa0beec8d7155077b4a4379eb
Yannic Schröder <schroeder@ibr.cs.tu-bs.de>
2017-05-11 16:22:07 +0200

    inphasebridge: major rework to use selector instead of busy waiting threads

--- inphasebridge/inphasebridge.py
+++ inphasebridge/inphasebridge.py
@@ -1,237 +1,178 @@
 #!/usr/bin/python3
-import socket
-import sys
-import threading
-import time
-import glob
-import subprocess
 
+import socket
+import selectors
+import glob
 import serial
+import subprocess
 
 import logging
 logging.basicConfig(level=logging.INFO)
 
-ser_lock = threading.Lock()  # lock for serial object between threads
-ser_conn = None              # serial connection object
+sel = selectors.DefaultSelector()
+
+IP = '0.0.0.0'
+SERIAL_PORT = 50000
+CONTROL_PORT = 50001
+
+SELECTOR_TIMEOUT = 1  # timeout in seconds for selector (float allowed)
+
+BAUDRATE = 38400
+RESET_PIN = 7
+
+serial_connections = list()  # this holds all serial connections for port multiplexing
+ser_conn = serial.Serial(None, BAUDRATE, timeout=0)
 
 
-# from: http://stackoverflow.com/questions/323972/is-there-any-way-to-kill-a-thread-in-python
-class StoppableThread(threading.Thread):
-    """Thread class with a stop() method. The thread itself has to check
-    regularly for the stopped() condition."""
+def getDevice():
+    usb_ports = glob.glob('/dev/ttyUSB*')
+    normal_ports = glob.glob('/dev/ttyS*')
 
-    def __init__(self):
-        super(StoppableThread, self).__init__()
-        self._stop_event = threading.Event()
+    # use USB serial device if available
+    if len(usb_ports) > 0:
+        return usb_ports[0]
+    elif len(normal_ports) > 0:
+        return normal_ports[0]
 
-    def stop(self):
-        self._stop_event.set()
-
-    def stopped(self):
-        return self._stop_event.is_set()
+    return None
 
 
-class SerialThread(StoppableThread):
-
-    def __init__(self, timeout=0.1):
-        super(SerialThread, self).__init__()
-        self.timeout = timeout
-        self.current_device = None
-        global ser_conn
-        ser_lock.acquire()
-        ser_conn = serial.Serial(None, 38400, timeout=self.timeout)
-        ser_lock.release()
-
-    def getDevice(self):
-        usb_ports = glob.glob('/dev/ttyUSB*')
-        normal_ports = glob.glob('/dev/ttyS*')
-
-        # use USB serial device if available
-        if len(usb_ports) > 0:
-            return usb_ports[0]
-        elif len(normal_ports) > 0:
-            return normal_ports[0]
-
-        return None
-
-    def run(self):
-        global ser_conn, ser_lock
-        while not self.stopped():
-            best_device = self.getDevice()
-            if best_device != self.current_device:
-                with ser_lock:
-                    if ser_conn.isOpen():
-                        logging.warn('Closing serial port %s', self.current_device)
-                        ser_conn.close()
-                    if best_device is None:
-                        logging.warn('No serial port found!')
-                    else:
-                        time.sleep(1)  # give udev a bit of time here to trigger
-                        ser_conn.port = best_device
-                        try:
-                            ser_conn.open()
-                            logging.info('Connected to serial port %s', best_device)
-                        except serial.serialutil.SerialException:
-                            # sometimes the device becomes unavailable between getDevice() and open()
-                            # just try again next time
-                            logging.warn('Could not connect to serial port %s', best_device)
-                    self.current_device = best_device
-            time.sleep(1)  # wait a bit to reduce CPU load
+def control_socket_accept(sock, mask):
+    conn, addr = sock.accept()  # Should be ready
+    logging.info('accepted control connection from %s', addr)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, control_socket_read)
 
 
-class TCPThread(StoppableThread):
+def control_socket_read(conn, mask):
+    data = conn.recv(1000)  # Should be ready
+    if data:
+        while b'\n' in data:
+            (line, data) = data.split(b'\n', 1)
+            line = line.rstrip()
+            line = line.upper()
 
-    def __init__(self, host, port):
-        super(TCPThread, self).__init__()
-        self.host = host
-        self.port = port
-        self.conn = None
-        self.s = None
-
-    def run(self):
-        while not self.stopped():
-            for res in socket.getaddrinfo(self.host, self.port, socket.AF_UNSPEC,
-                                          socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-                af, socktype, proto, canonname, sa = res
-                try:
-                    self.s = socket.socket(af, socktype, proto)
-                except OSError as msg:
-                    logging.error('OSError: %s', msg)
-                    self.s = None
+            if line == b'RESET':
+                if 'USB' in ser_conn.name:
+                    # TODO: reset via inga_tool if device is connected via USB
+                    conn.sendall(b'501 NOT IMPLEMENTED (usb reset not supported)\n')
                     continue
+                # when it is not a USB device, reset via gpio binary
                 try:
-                    self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)     # do not wait for TCP TIMED_WAIT
-                    self.s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)     # do not use nagle and send directly
-                    self.s.bind(sa)
-                    self.s.listen(1)
-                    logging.info('Listening on %s:%s', self.host, self.port)
-                except OSError as msg:
-                    logging.error('OSError: %s', msg)
-                    self.s.close()
-                    self.s = None
-                    continue
-                break
-            if self.s is None:
-                logging.error('could not open socket')
-                sys.exit(1)
-            self.s.setblocking(False)
-            while True:
-                if self.stopped():
-                    return 0
-                # wait for connection
-                try:
-                    self.conn, addr = self.s.accept()
-                except BlockingIOError:
-                    continue
-                break  # break if we have a connection
-            with self.conn:
-                self.conn.setblocking(False)                                             # non-blocking receive
-                logging.info('Connected: %s', addr)
-                self.loop()
-                logging.info('Disconnected: %s', addr)
-
-    def loop(self):
-        while not self.stopped():
-            time.sleep(0.1)
-
-
-class NetworkingThread(TCPThread):
-
-    def __init__(self, host=None, port=50000):
-        super(NetworkingThread, self).__init__(host, port)
-
-    def loop(self):
-        while not self.stopped():
-            time.sleep(0.1)
-            with ser_lock:
-                try:
-                    data = self.conn.recv(1024)
-                    if data:
-                        logging.debug('write to serial: %s', data)
-                        try:
-                            ser_conn.write(data)
-                        except serial.serialutil.SerialException as msg:
-                            logging.error('SerialException: %s', msg)
-                    data = ser_conn.read(1024)
-                    if data:
-                        logging.debug('read from serial: %s', data)
-                        try:
-                            self.conn.send(data)
-                        except BrokenPipeError:
-                            break
-                except BlockingIOError as msg:
-                    pass
-
-
-class ControlThread(TCPThread):
-
-    def __init__(self, host=None, port=50001, reset_pin=None):
-        super(ControlThread, self).__init__(host, port)
-        self.reset_pin = reset_pin
-
-    def loop(self):
-        data = bytearray()  # byte buffer that behaves like a file
-        while not self.stopped():
-            time.sleep(0.1)
-            try:
-                new_data = self.conn.recv(1024)
-            except BlockingIOError as msg:
-                # this means the socket is still open, but has no new data
-                pass
+                    subprocess.call(['gpio', 'mode', str(RESET_PIN), 'out'])
+                    subprocess.call(['gpio', 'write', str(RESET_PIN), '0'])
+                    subprocess.call(['gpio', 'mode', str(RESET_PIN), 'in'])
+                    conn.sendall(b'200 OK\n')
+                except FileNotFoundError:
+                    # this can fail on machines that do not have the gpio binary
+                    conn.sendall(b'501 NOT IMPLEMENTED (gpio binary missing)\n')
+            elif line == b'STATUS':
+                conn.sendall(b'Device: ' + bytes(ser_conn.name, encoding='ascii') + b'\n')
+                ser_status = b'True' if ser_conn.isOpen() else b'False'
+                conn.sendall(b'Connected: ' + ser_status + b'\n')
+                conn.sendall(b'Connected clients: ' + bytes(str(len(serial_connections)), encoding='ascii') + b'\n')
+                conn.sendall(b'200 OK\n')
             else:
-                if not new_data:
-                    # no new data means the socket is closed, return from this loop
-                    break
-                else:
-                    data += new_data
-            # process all lines found
-            while b'\n' in data:
-                (line, data) = data.split(b'\n', 1)
-                line = line.rstrip()
-                if line == b'RESET':
-                    if 'USB' in ser_conn.name:
-                        # TODO: reset via inga_tool if device is connected via USB
-                        self.conn.send(b'501 NOT IMPLEMENTED (usb reset not supported)\n')
-                        continue
-                    # when it is not a USB device, reset via gpio binary
-                    try:
-                        subprocess.call(['gpio', 'mode', str(self.reset_pin), 'out'])
-                        subprocess.call(['gpio', 'write', str(self.reset_pin), '0'])
-                        subprocess.call(['gpio', 'mode', str(self.reset_pin), 'in'])
-                        self.conn.send(b'200 OK\n')
-                    except FileNotFoundError:
-                        # this can fail on machines that do not have the gpio binary
-                        self.conn.send(b'501 NOT IMPLEMENTED (gpio binary missing)\n')
-                elif line == b'STATUS':
-                    self.conn.send(b'200 OK\n')
-                    with ser_lock:
-                        ser_status = b'True' if ser_conn.isOpen() else b'False'
-                        self.conn.send(b'Device: ' + bytes(ser_conn.name, encoding='utf-8') + b'\n')
-                        self.conn.send(b'Connected: ' + ser_status + b'\n')
-                else:
-                    self.conn.send(b'400 BAD REQUEST\n')
+                conn.sendall(b'400 BAD REQUEST\n')
+    else:
+        logging.info('closing %s', conn)
+        sel.unregister(conn)
+        conn.close()
+
+
+def serial_socket_accept(sock, mask):
+    conn, addr = sock.accept()  # Should be ready
+    logging.info('accepted serial connection from %s', addr)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, serial_socket_read)
+    serial_connections.append(conn)
+
+
+def serial_socket_read(conn, mask):
+    data = conn.recv(1000)  # Should be ready
+    if data:
+        for s in serial_connections:
+            if s == conn:
+                continue
+            s.sendall(data)
+        logging.debug('write to serial: %s', data)
+        try:
+            ser_conn.write(data)
+        except serial.serialutil.SerialException as msg:
+            logging.error('SerialException: %s', msg)
+    else:
+        logging.info('closing %s', conn)
+        sel.unregister(conn)
+        serial_connections.remove(conn)
+        conn.close()
+
+
+def serial_read(conn, mask):
+    try:
+        data = conn.read(1024)
+        for s in serial_connections:
+            s.sendall(data)
+    except serial.serialutil.SerialException:
+        logging.warn('Closing serial port %s', conn.port)
+        sel.unregister(conn)
+        conn.close()
 
 if __name__ == "__main__":
+    running = True
+
     import signal
 
     def signal_handler(signal, frame):
+        global running
         logging.info('Exiting...')
-        serial_thread.stop()
-        networking_thread.stop()
-        control_thread.stop()
+        running = False
 
     signal.signal(signal.SIGINT, signal_handler)
 
-    serial_thread = SerialThread()
-    networking_thread = NetworkingThread()
-    control_thread = ControlThread(reset_pin=7)
+    serial_sock = socket.socket()
+    serial_sock.bind((IP, SERIAL_PORT))
+    serial_sock.listen(1)
+    serial_sock.setblocking(False)
+    sel.register(serial_sock, selectors.EVENT_READ, serial_socket_accept)
+    logging.info('listening on %s:%s for serial connections', IP, SERIAL_PORT)
+    serial_conn = None
 
-    serial_thread.start()
-    networking_thread.start()
-    control_thread.start()
+    control_sock = socket.socket()
+    control_sock.bind((IP, CONTROL_PORT))
+    control_sock.listen(1)
+    control_sock.setblocking(False)
+    sel.register(control_sock, selectors.EVENT_READ, control_socket_accept)
+    logging.info('listening on %s:%s for control connections', IP, CONTROL_PORT)
 
-    serial_thread.join()
-    networking_thread.join()
-    control_thread.join()
+    current_device = None
 
-    logging.info('Goodbye!')
+    while running:
+        best_device = getDevice()
+
+        if best_device != current_device:
+            if ser_conn.isOpen():
+                logging.warn('Closing serial port %s', current_device)
+                sel.unregister(ser_conn)
+                ser_conn.close()
+            if best_device is None:
+                logging.warn('No serial port found!')
+            else:
+                ser_conn.port = best_device
+                try:
+                    ser_conn.open()
+                    sel.register(ser_conn, selectors.EVENT_READ, serial_read)
+                    logging.info('Connected to serial port %s', best_device)
+                    current_device = best_device
+                except serial.serialutil.SerialException:
+                    # sometimes the device becomes unavailable between getDevice() and open()
+                    # just try again next time
+                    logging.warn('Could not connect to serial port %s', best_device)
+
+        events = sel.select(SELECTOR_TIMEOUT)
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
+
+    ser_conn.close()
+    control_sock.close()
+    serial_sock.close()
