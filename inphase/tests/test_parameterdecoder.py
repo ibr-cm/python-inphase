5a28bdb68bebce96825fe5caa1c3a6f4a90ef6e0
Dennis Reimers <d.reimers@tu-bs.de>
2017-06-20 12:09:34 +0200

    parameterdecoder: add test for false-positive matches
    false-postives:
    
    strings that are parsed as parameters but they just contain an '.'

--- inphase/tests/test_parameterdecoder.py
+++ inphase/tests/test_parameterdecoder.py
@@ -43,6 +43,34 @@ class UnitTest(unittest.TestCase):
         self.assertEqual(len(parameters), 1)
         self.assertDictEqual(parameters, {'test_key': 1234})
 
+    def test_parsing_false_positive(self):
+        remaining_data = bytes()
+        parameters = dict()
+        test_data_unmatch = b'.distance_status: DISTANCE_RUNNING\r\n'
+        test_data_unmatch += b'..distance_status: DISTANCE_RUNNING\r\n'
+        test_data_unmatch += b'sensor..param1: value1\r\n'
+        test_data_unmatch += b'sensor..param1.subparam1: subvalue1\r\n'
+        test_data_unmatch += b'sensor..param1.subparam1: subvalue1\r\n'
+        remaining_data = test_data_unmatch
+        parameters_found, remaining_data, clean = inphase.decodeParameters(remaining_data)
+        parameters.update(parameters_found)
+        self.assertEqual(len(parameters), 0)
+        self.assertEqual(clean, test_data_unmatch)
+        self.assertEqual(remaining_data, b'')
+
+        test_data_match = b'param1: value1\r\n'
+        test_data_match += b'sensor.param1: value1\r\n'
+        test_data_match += b'sensor.param1.subparam1: subvalue1\r\n'
+        test_data_match += b'sensor.param1.subparam1.sub1: subsubvalue1\r\n'
+
+        remaining_data = test_data_match
+        parameters_found, remaining_data, clean = inphase.decodeParameters(remaining_data)
+        parameters.update(parameters_found)
+        self.assertEqual(len(parameters), 4)
+        self.assertEqual(clean, b'')
+        self.assertEqual(remaining_data, b'')
+
+
     def test_parsing_dump(self):
         # split serial data into pieces of 10 bytes
         data_pieces = [self.serial_dump[i:i+10] for i in range(0, len(self.serial_dump), 10)]
@@ -55,8 +83,7 @@ class UnitTest(unittest.TestCase):
             parameters.update(p)
             if not p:
                 self.assertEqual(c+remaining_data, remaining_data_before)
-        # TODO: there is only on 'real' parameter in testdata, but currenly some other output also matches
-        self.assertEqual(len(parameters), 2)
+        self.assertEqual(len(parameters), 1)
         self.assertIn('distance_sensor0.start', parameters)
         self.assertEqual(parameters['distance_sensor0.start'], 1)
 
