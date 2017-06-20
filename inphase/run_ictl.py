71ef85aece4c87ae171b12febef6d7f59f4085d8
Dennis Reimers <d.reimers@tu-bs.de>
2017-06-20 15:22:14 +0200

    utils: use socket mode of provider

--- inphase/run_ictl.py
+++ inphase/run_ictl.py
@@ -18,16 +18,18 @@ def handler(signum, frame):
 
 running = True
 mytarget = 0xdb98
-provider = InphasectlMeasurementProvider('/dev/ttyUSB0', count=6, target=mytarget)
+# provider = InphasectlMeasurementProvider('/dev/ttyUSB0', count=6, target=mytarget)
+bridge_provider = InphasectlMeasurementProvider(address='localhost', count=6, target=mytarget)
+provider = bridge_provider
 signal.signal(signal.SIGINT, handler)
 
 all_measurements = list()
 
 # while running:
-for x in range(5):
+for x in range(3):
     measurements = provider.getMeasurements()
     all_measurements += measurements
-    print("(%d/%d) target %x got %s measurements. Successful %d of %d" % (x+1, 5, mytarget, len(measurements), len(all_measurements), (x+1)*6))
+    print("(%d/%d) target %x got %s measurements. Successful %d of %d" % (x+1, 3, mytarget, len(measurements), len(all_measurements), (x+1)*6))
     print("sleep 2 seconds")
     time.sleep(2)
 
