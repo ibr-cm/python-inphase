2efe2032e825632d9bdbecbddbdb8159427484ef
Dennis Reimers <d.reimers@tu-bs.de>
2017-06-20 12:26:03 +0200

    utils: fix counter of all measurements

--- inphase/run_ictl.py
+++ inphase/run_ictl.py
@@ -27,7 +27,7 @@ all_measurements = list()
 for x in range(5):
     measurements = provider.getMeasurements()
     all_measurements += measurements
-    print("(%d/%d) target %x got %s measurements. Successful %d of %d" % (x+1, 5, mytarget, len(measurements), len(all_measurements), (x+1)*5))
+    print("(%d/%d) target %x got %s measurements. Successful %d of %d" % (x+1, 5, mytarget, len(measurements), len(all_measurements), (x+1)*6))
     print("sleep 2 seconds")
     time.sleep(2)
 
