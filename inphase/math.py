ed182ad1b43d5877b9bc46748adf0a81c6a40dcb
Dennis Reimers <d.reimers@tu-bs.de>
2018-01-26 10:08:54 +0100

    math: add refactored slope2dist, normalization, doc

--- inphase/math.py
+++ inphase/math.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """Math functions.
 
-This module contains math functions that are needed to work with InPhase measurement data.
+This module contains math functions that are needed to work with InPhase
+measurement data.
 
 """
 
 import numpy as np
 
 from inphase.interpolation import parabolic
-from inphase.constants import MAX_DISTANCE
+from inphase.constants import SPEED_OF_LIGHT, DEFAULT_FREQ_SPACING
 
 DEFAULT_FFT_LEN = 4096
+DEFAULT_DC_TRESHOLD = 0
 
 
 def calculateDistance(measurement, calc_type='complex', interpolation=None, **kwargs):
@@ -18,11 +22,10 @@ def calculateDistance(measurement, calc_type='complex', interpolation=None, **kw
     Args:
         measurement (:obj:`Measurement`): The measurement to calculate a distance for.
         calc_type (str, optional):  Algorithm to use for calculation:
+        `calc_type` can be one the following options:
+                * `real` will use the algorithm published in our `INFOCOM paper`_
+                * `complex` results in the more robust calculation via a complex valued FFT and allows double maximum distance.
         interpolation (string): Method of spectral interpolation, set value will be passed to interpolation function.
-
-    `calc_type` can be one the following options:
-            * `real` will use the algorithm published in our `INFOCOM paper`_
-            * `complex` results in the more robust calculation via a complex valued FFT and allows double maximum distance, also allows usage of 'dc_threshold'
 
     Keyword Arguments:
         fft_bins (int): Number of FFT bins, more FFT bins result in higher resolution of the result.
@@ -38,53 +41,60 @@ def calculateDistance(measurement, calc_type='complex', interpolation=None, **kw
         https://www.ibr.cs.tu-bs.de/bib/xml/vonzengen:INFOCOM2016.html
     """
     extra_data = dict()
+    maxima = list()
 
     fft_bins = kwargs.get('fft_bins', DEFAULT_FFT_LEN)
-    dc_threshold = kwargs.get('dc_threshold', 0)
+    dc_threshold = kwargs.get('dc_threshold', DEFAULT_DC_TRESHOLD)
 
     fft_result, fft_extras = calc_fft_spectrum(measurement, calc_type, fft_bins)
-
-    if calc_type == 'real':
-        fft_length = fft_bins
-    elif calc_type == 'complex':
-        fft_length = len(fft_result)
 
     # search maxima
     # TODO implement multi maxima handling
     bin_pos = np.argmax(fft_result)
     bin_value = np.max(fft_result)
 
+    # interpolate spectrum around maxima
     if interpolation:
         bin_pos, bin_value = _interpolate_maxima_position(fft_result,
                                                           bin_pos,
                                                           mode=interpolation)
 
-    if bin_pos < dc_threshold or bin_pos > fft_bins - dc_threshold:
+    if _in_dc_threshold(bin_pos, dc_threshold, fft_bins):
+        # block positions around 0
         norm_bin_pos = np.nan
-    else:
-        # normalize bin position
-        norm_bin_pos = _normalize_bin_pos(bin_pos, calc_type, fft_length)
-
-    if np.isnan(norm_bin_pos):
         extra_data['dqi'] = 0
         distance = np.nan
     else:
+        # normalize bin position
+        fft_length = len(fft_result)
+        norm_bin_pos = _normalize_bin_pos(bin_pos, fft_length)
         # store bin value as dqi
         extra_data['dqi'] = bin_value
         # calculate distance from bin position
         if calc_type == 'real':
-            distance = _slopeToDist(norm_bin_pos)
+            # real fft calculation reduces d_max to the half
+            distance = _slope_to_dist(norm_bin_pos, half_d_max=True)
         elif calc_type == 'complex':
-            distance = _slopeToDist2(norm_bin_pos)
+            distance = _slope_to_dist(norm_bin_pos)
 
-    # store extra data of FFT
-    extra_data['fft'] = fft_result
-    extra_data.update(fft_extras)
+    maximum = bin_pos, bin_value
+    maxima.append(maximum)
 
     # subtract antenna offsets if provided
     distance = substract_provided_offsets(measurement, distance)
 
+    # store extra data of FFT
+    extra_data['fft'] = fft_result
+    extra_data.update(fft_extras)
+    # store maxima in extra_data
+    extra_data['maxima'] = maxima
+
     return distance, extra_data
+
+
+def _in_dc_threshold(bin_pos, dc_threshold, fft_bins):
+    """Return whether a bin position is in [-dc_threshold, dc_threshold]"""
+    return bin_pos < dc_threshold or bin_pos > fft_bins - dc_threshold
 
 
 def _interpolate_maxima_position(fft, maximum, mode):
@@ -157,14 +167,9 @@ def calc_fft_spectrum(measurement, calc_type, fft_bins=DEFAULT_FFT_LEN):
     return fft_result, extra_data
 
 
-def _normalize_bin_pos(bin_pos, calc_type, fft_bins=DEFAULT_FFT_LEN):
+def _normalize_bin_pos(bin_pos, fft_bins=DEFAULT_FFT_LEN):
     """find bin with maximum peak and normalize to [0, 1]"""
-    if calc_type == 'real':
-        norm_bin_pos = bin_pos / float(fft_bins / 2.0)
-    elif calc_type == 'complex':
-        norm_bin_pos = bin_pos / fft_bins
-
-    return norm_bin_pos
+    return bin_pos / fft_bins
 
 
 def substract_provided_offsets(measurement, distance):
@@ -181,16 +186,23 @@ def substract_provided_offsets(measurement, distance):
     return distance
 
 
-def _slopeToDist(m, fd=0.5):
-    # fd is the sample spacing in MHz
-    c = 299792458                       # speed of light
-    wavelength = c / (float(fd) * 10**6) * 0.5   # effective wavelength
-    d_max = wavelength / 2                       # maximum distance with given frequency delta
-    return d_max * m * 1000             # return value in millimeter
+def _slope_to_dist(m, fd=DEFAULT_FREQ_SPACING, half_d_max=False):
+    """Calculate distance from slope of phase response.
 
+    Args:
+        m (float): The normalized bin position in [0, 1]
+        fd (float, optional): fd is the sample spacing in MHz
+        half_d_max (boolean, optional): If set true the maximum distance
+                                        is halved
 
-def _slopeToDist2(m, fd=0.5):
-    # fd is the sample spacing in MHz
-    c = 299792458                       # speed of light
-    wavelength = c / (float(fd) * 10**6) * 0.5   # effective wavelength and maximum distance
-    return wavelength * m * 1000                 # return value in millimeter
+    Returns:
+        * distance in millimeter (float)
+    """
+    c = SPEED_OF_LIGHT                          # speed of light c = 299792458 m/s
+    wavelength = c / (float(fd) * 10**6) * 0.5  # effective wavelength in meter
+    if half_d_max:
+        d_max = wavelength / 2
+    else:
+        d_max = wavelength
+
+    return d_max * m * 1000                     # return value in millimeter
