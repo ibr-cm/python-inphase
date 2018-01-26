a208f66644f50b5b297948444a5c100b06bcf58e
Dennis Reimers <d.reimers@tu-bs.de>
2018-01-26 10:06:44 +0100

    constants: add docstring

--- inphase/constants.py
+++ inphase/constants.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Collection of contants used in calculations."""
+
 SPEED_OF_LIGHT = 299792458  # speed of light
 DEFAULT_FREQ_SPACING = 0.5  # Step between two frequencies in MHz
 MAX_DISTANCE = SPEED_OF_LIGHT / (float(DEFAULT_FREQ_SPACING) * 10**6) * 0.5  # effective wavelength and maximum distance
