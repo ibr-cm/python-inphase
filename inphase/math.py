Fix dvss spectrum calculation

inphase/math.py
@@ -11,7 +11,8 @@ from scipy.signal import argrelmax
 import numpy as np
 
 from inphase.interpolation import parabolic
-from inphase.constants import SPEED_OF_LIGHT, DEFAULT_FREQ_SPACING
+from inphase.constants import SPEED_OF_LIGHT, DEFAULT_FREQ_SPACING, MAX_DISTANCE
+from inphase.slope_sampling import calc_dvss_spectrum
 
 DEFAULT_FFT_LEN = 4096
 DEFAULT_DC_TRESHOLD = 0
@@ -102,6 +103,9 @@ def calculateDistances(measurement, calc_type='complex', interpolation=None, mul
                 distance = _slope_to_dist(norm_bin_pos)
             elif calc_type == 'complex_with_magnitude':
                 distance = _slope_to_dist(norm_bin_pos)
+            elif calc_type == 'dvss':
+                # TODO check this multi value??
+                distance = _slope_to_dist(norm_bin_pos, half_d_max=True)
 
         # subtract antenna offsets if provided
         distance = substract_provided_offsets(measurement, distance)
@@ -223,6 +227,11 @@ def calc_fft_spectrum(measurement, calc_type, fft_bins=DEFAULT_FFT_LEN):
             # maximum positive and minimum negative frequency are aliases,
             # remove the minumum negative frequency
             fft_result = np.delete(fft_result, int(fft_bins / 2))
+    elif calc_type == 'dvss':
+        # calculate max_dist and resolution to get sample slopes simliar to fft bins
+        max_distance = MAX_DISTANCE
+        fft_result = calc_dvss_spectrum(measurement, max_distance,
+                                        resolution=fft_bins)
     else:
         raise NotImplementedError('The chosen calc_type does not exist!')
 

inphase/slope_sampling.py
@@ -12,6 +12,9 @@ import peakutils.peak
 
 from inphase.constants import SPEED_OF_LIGHT
 
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+
 
 def prepare_pmu_samples(measurement):
     """Convert inphase measurement to phase data
@@ -33,6 +36,20 @@ def prepare_pmu_samples(measurement):
     delta_phi = 2*np.pi*1/256*pmu_data.T[1]
 
     return delta_phi, delta_f, frequency_step
+
+
+def calc_dvss_spectrum(measurement, max_distance=30, resolution=3001, cut=None,
+                       conf_rel=0, res_tol=1):
+    d_samples = np.linspace(0, max_distance, resolution)
+    delta_phi, delta_f, freq_step = prepare_pmu_samples(measurement)
+    # plt.plot(delta_phi, label='delta phi from ideal pmu')
+    # plt.legend()
+    # plt.show()
+
+    spectrum = dvss_spectrum(delta_phi, delta_f, freq_step, cut,
+                             d_samples, conf_rel, res_tol)
+
+    return spectrum
 
 
 def calc_dist_via_slope_sampling(measurement, max_distance=30, resolution=3001, cut=None,
@@ -112,7 +129,6 @@ def _calc_A_B_matrices(nr_samples, nr_comb, delta_phis, m_samples):
 
 def _calc_residues(A_Delta, B_sDelta, m_samples, nr_comb):
     """Calculate residues between measured slopes and sample slopes."""
-    # ref_data = loadmat('DVSS1.mat')
     # eq 12 inner
     residues = A_Delta.T - B_sDelta
     # eq 12 outer
@@ -121,6 +137,29 @@ def _calc_residues(A_Delta, B_sDelta, m_samples, nr_comb):
     Rmin = np.reshape(res_min, (len(m_samples), nr_comb)).T
 
     return Rmin
+
+
+def dvss_spectrum(delta_phi, delta_f, df, cut, d_samples, conf_rel, res_tol):
+    delta_phis, nr_comb = _calc_delta_phi(delta_phi, delta_f, df)
+    if cut:
+        freq_include = round(cut * nr_comb / 100)
+    else:
+        freq_include = nr_comb-1  # all frequencies
+
+    m_samples = _calc_sample_slopes(d_samples)
+
+    A_Delta, B_sDelta = _calc_A_B_matrices(len(d_samples), nr_comb, delta_phis, m_samples)
+    Rmin = _calc_residues(A_Delta, B_sDelta, m_samples, nr_comb)
+    sort_res_min = np.sort(Rmin, 0)
+    # eq 14
+    res = np.median(sort_res_min[0:freq_include], 0)
+    frame_length = int(np.ceil(len(res)/10))
+    frame_length = 211
+    # residues_filtered = savgol_filter(res, frame_length, 7)
+    residues_filtered = res
+
+    spectrum = 1/residues_filtered
+    return spectrum
 
 
 def slope_sampling(delta_phi, delta_f, df, cut, d_samples, conf_rel, res_tol):
@@ -135,50 +174,70 @@ def slope_sampling(delta_phi, delta_f, df, cut, d_samples, conf_rel, res_tol):
 
     m_samples = _calc_sample_slopes(d_samples)
 
+    ref_data = loadmat('DVSS1.mat')
+
     A_Delta, B_sDelta = _calc_A_B_matrices(len(d_samples), nr_comb, delta_phis, m_samples)
     Rmin = _calc_residues(A_Delta, B_sDelta, m_samples, nr_comb)
 
-    # print("Check res_min against ref_data: \t", np.allclose(Rmin, ref_data['res_min']))
+    print("Check res_min against ref_data: \t", np.allclose(Rmin, ref_data['res_min']))
 
-    # del ref_data
-    # ref_data = loadmat('DSS1b.mat')
+    del ref_data
+    ref_data = loadmat('DSS1b.mat')
     sort_res_min = np.sort(Rmin, 0)
     # eq 14
     res = np.median(sort_res_min[0:freq_include], 0)
-    # print("Check sort_res_min against ref_data: \t", np.allclose(sort_res_min, ref_data['sort_res_min']))
-    # print("Check res against ref_data: \t", np.allclose(res, ref_data['res_median']))
+    print("Check sort_res_min against ref_data: \t", np.allclose(sort_res_min, ref_data['sort_res_min']))
+    print("Check res against ref_data: \t", np.allclose(res, ref_data['res_median']))
     frame_length = int(np.ceil(len(res)/10))
     frame_length = 211
     res_filt = savgol_filter(res, frame_length, 7)
-    # print("Check res_filt against ref_data: \t", np.allclose(res_filt, ref_data['res'][0]))
-    # plt.plot(res, '-o', label='res_median')
-    # plt.plot(res_filt, '-+', label='res_filt')
-    # # plt.plot(ref_data['res_median'][0], '-s', label='ref_data res_median')
-    # plt.plot(ref_data['res'][0], '-s', label='ref_data res')
-    # plt.legend()
+    print("Check res_filt against ref_data: \t", np.allclose(res_filt, ref_data['res'][0]))
+    plt.plot(res, '-o', label='res_median')
+    plt.plot(res_filt, '-+', label='res_filt')
+    plt.plot(ref_data['res_median'][0], '-s', label='ref_data res_median')
+    plt.plot(ref_data['res'][0], '-s', label='ref_data res')
+    plt.legend()
     # plt.show()
     # NOTE: workaround use filtered ref data
     # res_filt = ref_data['res'][0]
+    plt.plot(res, label='res')
+    plt.plot(res_filt, label='res_filt')
+    plt.legend()
+    plt.show()
     res = res_filt
+
     # Distance selection using Peak Search
     if res_tol == 0:
         res_tol = 1
     else:
         res_tol = (len(d_samples) - 1) / max(d_samples) * res_tol
 
+
     max_res = np.max(1/res)
     res_index = peakutils.peak.indexes(1/res, thres=0.3/max_res,
                                        min_dist=res_tol)
     conf_res = (1/res)[res_index]
-    # print("Check res_index against ref_data: \t", np.allclose(res_index, ref_data['res_index'][0] - 1))
+    print("Check res_tol against ref_data: \t", float(ref_data['res_tol'][0][0]) == res_tol)
+
+
+    plt.plot(1/res_filt, label='res_filt')
+    plt.plot(1/ref_data['res'][0], '-s', label='ref_data res')
+    plt.plot(res_index, conf_res, label='Found Peaks')
+    import ipdb; ipdb.set_trace()
+    ref_conf_res = 1/(ref_data['res'][0][ref_data['res_index']])
+    plt.plot(ref_data['res_index'][0], ref_conf_res[0], label='ref Peaks')
+    plt.legend()
+    plt.show()
+
+    print("Check res_index against ref_data: \t", np.allclose(res_index, ref_data['res_index'][0] - 1))
 
     # sort descending and return args
     res_index1 = np.argsort(conf_res)[::-1]
     conf_res = conf_res[res_index1]
-    # print("Check conf_res against ref_data: \t", np.allclose(conf_res, ref_data['conf_res'][0]))
+    print("Check conf_res against ref_data: \t", np.allclose(conf_res, ref_data['conf_res'][0]))
 
-    # del ref_data
-    # ref_data = loadmat('DSSV1c.mat')
+    del ref_data
+    ref_data = loadmat('DSSV1c.mat')
     a = 1
     i = 1
     while i <= len(conf_res) and a > conf_rel:
@@ -192,14 +251,14 @@ def slope_sampling(delta_phi, delta_f, df, cut, d_samples, conf_rel, res_tol):
             residue = res[res_index[res_index1[0:i]]].T
             D = [distance, residue, d_conf.T]
         i = i + 1
-    # print("Check d_conf_pre_norm against ref_data: \t", np.allclose(d_conf_pre_norm, ref_data['d_conf_pre_norm'][0]))
-    # print("Check d_conf against ref_data: \t", np.allclose(d_conf, ref_data['d_conf'][0]))
-    # print("Check distance against ref_data: \t", np.allclose(distance, ref_data['D'].T[0]))
-    # print("Check residue against ref_data: \t", np.allclose(residue, ref_data['D'].T[1]))
+    print("Check d_conf_pre_norm against ref_data: \t", np.allclose(d_conf_pre_norm, ref_data['d_conf_pre_norm'][0]))
+    print("Check d_conf against ref_data: \t", np.allclose(d_conf, ref_data['d_conf'][0]))
+    print("Check distance against ref_data: \t", np.allclose(distance, ref_data['D'].T[0]))
+    print("Check residue against ref_data: \t", np.allclose(residue, ref_data['D'].T[1]))
 
     cdists = distance
     extra = {'dqi': np.max(d_conf),
-             'dqis': d_conf,
+             'dqis': np.array(d_conf),
              'd_samples': d_samples,
              'res': res,
              'D_res': residue}